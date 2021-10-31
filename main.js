4ea9af567f1287aa6ec28a3cc46269835e2d548f
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2021-10-31 17:35:27 -0400

    removido alterações de animação css e add responsividade

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const LinksSocialMedia = {
-  github: "Wanderson-A-Timoteo",
-  youtube: "channel/UCYnS7pxwA-io-jWU78z1eVQ",
-  linkedin: "in/wanderson-a-timoteo",
-  instagram: "wanderson_a_timoteo",
-  twitch: "wanderson_timoteo"
+  github: 'Wanderson-A-Timoteo',
+  youtube: 'channel/UCYnS7pxwA-io-jWU78z1eVQ',
+  facebook: 'wanderson.a.timoteo',
+  instagram: 'wanderson_a_timoteo',
+  twitter: 'wanderson_timoteo'
 }
 
 function changeSocialMediaLinks() {
@@ -11,19 +11,18 @@ function changeSocialMediaLinks() {
   // Pega children "filhos" do UL definido com id=socialLinks e armazena na variavel li.
   for (let li of socialLinks.children) {
 
-     // Pega o atributo class de cada li e armazena na variavel social
-    const social = li.getAttribute('class');
+    // Pega o atributo class de cada li e armazena na variavel social
+    const social = li.getAttribute('class')
 
-    // Pega children de li na posição 0, href e forma o link para acessar.
+     // Pega children de li na posição 0, href e forma o link para acessar.
     li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
-
   }
 }
 
 // Chama a função
 changeSocialMediaLinks()
 
-function getGithubProfileInfos() {
+function getGitHubProfileInfos() {
   const url = `https://api.github.com/users/${LinksSocialMedia.github}`
 
   fetch(url)
@@ -32,10 +31,10 @@ function getGithubProfileInfos() {
       userName.textContent = data.name
       userBio.textContent = data.bio
       userLink.href = data.html_url
-      userImage.src = data.avatar_url
-      userLogin.href = data.login
+      UserImage.src = data.avatar_url
+      userLogin.textContent = data.login
     })
 }
 
 // Chama a função
-getGithubProfileInfos()
+getGitHubProfileInfos()
